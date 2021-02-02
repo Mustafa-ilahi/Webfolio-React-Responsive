fd7aa7d5690cfbf5b8da44ba86893348c4ccb51f
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-03 02:37:09 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component{
   render(){
     return(
-      <div>
-        <h1>hello</h1>
-      </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Mustafa Ilahi</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Home</Nav.Link>
+      <Nav.Link href="#pricing">About</Nav.Link>
+      <Nav.Link href="#pricing">Skills</Nav.Link>
+      <Nav.Link href="#pricing">Experience</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     )
   }
 }
