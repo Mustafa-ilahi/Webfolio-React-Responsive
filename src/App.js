9d217997c28fc46a1ec804fe6538764940f4331b
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-03 04:17:35 +0500

    nav complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import devLogo from './assets/logo.png';
-import homeLogo from './assets/home.png';
-import aboutLogo from './assets/aboutLogo.png';
-import skillsLogo from './assets/skills.png';
-import experienceLogo from './assets/experience.png'
+import Nav_bar from './components/Navbar'
 
 class App extends React.Component{
   render(){
     return(
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="#home">
-      <img src={devLogo} className="devLogo"/>  
-        Mustafa Ilahi &nbsp; &nbsp;</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">
-          <img src={homeLogo} className="homeLogo"/>&nbsp; Home &nbsp;</Nav.Link>
-          <Nav.Link href="#pricing">
-          <img src={aboutLogo} className="aboutLogo"/>&nbsp; About &nbsp;</Nav.Link>
-          <Nav.Link href="#pricing">
-          <img src={skillsLogo} className="skillsLogo"/>&nbsp; Skills &nbsp;</Nav.Link>
-          <Nav.Link href="#pricing">
-          <img src={experienceLogo} className="experienceLogo"/>&nbsp; Experience</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      </Navbar>
+      <Nav_bar />
     )
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import '../App.css';
+import React from 'react';
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import devLogo from '../assets/logo.png';
+import homeLogo from '../assets/home.png';
+import aboutLogo from '../assets/aboutLogo.png';
+import skillsLogo from '../assets/skills.png';
+import experienceLogo from '../assets/experience.png'
+
+class Nav_bar extends React.Component{
+  render(){
+    return(
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Brand href="#home">
+      <img src={devLogo} className="devLogo"/>  
+        Mustafa Ilahi &nbsp; &nbsp;</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">
+          <img src={homeLogo} className="homeLogo"/>&nbsp; Home &nbsp;</Nav.Link>
+          <Nav.Link href="#pricing">
+          <img src={aboutLogo} className="aboutLogo"/>&nbsp; About &nbsp;</Nav.Link>
+          <Nav.Link href="#pricing">
+          <img src={skillsLogo} className="skillsLogo"/>&nbsp; Skills &nbsp;</Nav.Link>
+          <Nav.Link href="#pricing">
+          <img src={experienceLogo} className="experienceLogo"/>&nbsp; Experience</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
+
+export default Nav_bar;
