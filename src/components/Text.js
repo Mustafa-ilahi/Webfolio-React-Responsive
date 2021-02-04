f50e8b7541bd339123b1227c2bead95ed1bce090
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-04 06:35:04 +0500

    text updated

--- src/components/Text.js
+++ src/components/Text.js
@@ -8,7 +8,7 @@ import gmailIcon from '../assets/gmail.png';
 import fbIcon from '../assets/facebook.png';
 import scIcon from '../assets/sc.png';
 import instaIcon from '../assets/insta.png';
-import twitterIcon from '../assets/twitter.png';
+import codeIcon from '../assets/code.png';
 
 class Text extends React.Component{
     render(){
@@ -25,17 +25,43 @@ class Text extends React.Component{
                 <br/>
                 <br/>
                 <h4>Connect With Me Now!</h4>
+                <br/>
                 <a href="https://github.com/Mustafa-ilahi" target="_blank">
                 <img src={githubIcon} className="githubIcon"/> &nbsp; &nbsp; 
                 </a>
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=mustafailahi586@gmail.com&body=my-text" target="_blank">
                 <img src={gmailIcon} className="gmailIcon"/> &nbsp; &nbsp;
+                </a>
+                <a href="https://www.facebook.com/mustafa.ilahi.1/" target="_blank">
                 <img src={fbIcon} className="fbIcon"/> &nbsp; &nbsp;
+                </a>
+                <a href="https://www.snapchat.com/add/mustafa_ilahi" target="_blank">
                 <img src={scIcon} className="scIcon"/> &nbsp; &nbsp;
+                </a>
+                <a href="https://www.instagram.com/_mustafa_ilahi/" target="_blank">
                 <img src={instaIcon} className="instaIcon"/>  
+                </a>
                 <br/>
                 <br/>
+                <h5>Developer | Software Engineer</h5>
+                <br/>
+                <img src={codeIcon} className="codeIcon"/>
                 <br/>
                 <br/>
+                {/* Download Button */}
+                <a className="face-button" href="#">
+                <div className="face-primary">
+                    <span className="icon fa fa-cloud" />
+                    Download CV
+                </div>
+                <div className="face-secondary">
+                    <span className="icon fa fa-hdd-o" />
+                    Size: 52 kt
+                </div>
+                </a>
+                <hr />
+                <br />
+                <br />
              </div>
         )
     }
