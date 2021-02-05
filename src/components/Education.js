d3369ad68e167a81d27f136cf8d82caa6697e4ea
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-06 04:00:13 +0500

    education completed

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,6 +6,7 @@ import {Card,CardDeck} from 'react-bootstrap';
 import schoolImg from '../assets/school.jpg';
 import collegeImg from '../assets/college.jpeg';
 import uniImg from '../assets/uni.jpg';
+import lineImg from '../assets/line.PNG';
 
 class Education extends React.Component{
     render(){
@@ -22,19 +23,20 @@ class Education extends React.Component{
                 {/* Card */}
                 <div id="main">
                 <div id="one">
-                    <img src={uniImg} id="uniImg"/>
+                    <img src={schoolImg} id="uniImg"/>
                     <h4>Shah Grammar High School</h4>
+                    <br/>
                     <ul>
-                        <hr/>
+                        <img src={lineImg} id="lineImg"/>
                         <li><b>Science </b></li>
                         <li>Pass Out in 2016</li>
                     </ul>
                 </div>
                 <div id="two">
-                    <img src={uniImg} id="uniImg"/>
+                    <img src={collegeImg} id="uniImg"/>
                     <h4>P.E.C.H.S Govt Degree Science College</h4>
                     <ul>
-                        <hr/>
+                        <img src={lineImg} id="lineImg"/>
                         <li><b>Pre Engineering  </b></li>
                         <li>2016-2018</li>
                     </ul>
@@ -43,12 +45,13 @@ class Education extends React.Component{
                     <img src={uniImg} id="uniImg"/>
                     <h4>Sir Syed University of Technology and Engineering</h4>
                     <ul>
-                        <hr/>
+                        <img src={lineImg} id="lineImg"/>
                         <li><b>Bachelor of Science in Software Engineering</b></li>
                         <li>2019-2022</li>
                     </ul>
                 </div>
             </div>
+            <br/>
             </div>
             
         )
