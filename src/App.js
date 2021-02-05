d32a9bc968c2c1ea9a7af763748d8df77d5581bc
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-06 04:10:53 +0500

    education completed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav_bar from './components/Navbar';
 import Header from './components/Header';
-import Text from './components/Text'
+import Text from './components/Text';
 import PersonalInformation from './components/PersonalInformation';
-import Education from './components/Education'
+import Education from './components/Education';
+import Skills from './components/Skills';
 
 class App extends React.Component{
   render(){
@@ -17,6 +18,7 @@ class App extends React.Component{
         <Text />
         <PersonalInformation />
         <Education />
+        <Skills />
       </div>
     )
   }
---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import {React,Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Skills extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Mai skills hun</h1>
+            </div>
+        )
+    }
+}
+
+export default Skills;
