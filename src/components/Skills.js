2bf853c5ee9e8951fcfa9c6452cb2a3db90eb748
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-08 02:05:46 +0500

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,49 +1,49 @@
-import '../App.css';
-import {React,Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../App.css';
+// import {React,Component} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Skills extends Component{
-    render(){
-        return(
-            <div className="skillsDiv" id="skills">
-                <hr/>
-                <h3>Programming Languages that I Know</h3>
-                <br/>
-                <h4 style={{fontFamily:'sans-serif'}}>JavaScript</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-1" className="progress-bar" role="progressbar" style={{width:'95%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowOne"></div>
-                </div><br />
-                <h4 style={{fontFamily:'sans-serif'}}>React</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-2" className="progress-bar" role="progressbar" style={{width:'89%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowTwo"></div>
-                </div><br />
-                <h4 style={{fontFamily:'sans-serif'}}>Redux</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-3" className="progress-bar" role="progressbar" style={{width:'25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowThree"></div>
-                </div><br />
-                <h4 style={{fontFamily:'sans-serif'}}>C</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-4" className="progress-bar" role="progressbar" style={{width:'50%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowFour"></div>
-                </div><br />
-                <h4 style={{fontFamily:'sans-serif'}}>Java</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-5" className="progress-bar" role="progressbar" style={{width:'55%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowFive"></div>
-                </div><br />
-                <h4 style={{fontFamily:'sans-serif'}}>Python</h4>
-                <div id="prog" className="progress">
-                    <div id="prog-bar-6" className="progress-bar" role="progressbar" style={{width:'55%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    <div id="shadowSix"></div>
-                </div>
-                <br/>
-                <hr/>
-            </div>
-        )
-    }
-}
+// class Skills extends Component{
+//     render(){
+//         return(
+//             <div className="skillsDiv" id="skills">
+//                 <hr/>
+//                 <h3>Programming Languages that I Know</h3>
+//                 <br/>
+//                 <h4 style={{fontFamily:'sans-serif'}}>JavaScript</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-1" className="progress-bar" role="progressbar" style={{width:'95%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowOne"></div>
+//                 </div><br />
+//                 <h4 style={{fontFamily:'sans-serif'}}>React</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-2" className="progress-bar" role="progressbar" style={{width:'89%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowTwo"></div>
+//                 </div><br />
+//                 <h4 style={{fontFamily:'sans-serif'}}>Redux</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-3" className="progress-bar" role="progressbar" style={{width:'25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowThree"></div>
+//                 </div><br />
+//                 <h4 style={{fontFamily:'sans-serif'}}>C</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-4" className="progress-bar" role="progressbar" style={{width:'50%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowFour"></div>
+//                 </div><br />
+//                 <h4 style={{fontFamily:'sans-serif'}}>Java</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-5" className="progress-bar" role="progressbar" style={{width:'55%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowFive"></div>
+//                 </div><br />
+//                 <h4 style={{fontFamily:'sans-serif'}}>Python</h4>
+//                 <div id="prog" className="progress">
+//                     <div id="prog-bar-6" className="progress-bar" role="progressbar" style={{width:'55%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+//                     <div id="shadowSix"></div>
+//                 </div>
+//                 <br/>
+//                 <hr/>
+//             </div>
+//         )
+//     }
+// }
 
-export default Skills;
+// export default Skills;
